7b3a5e77d760d3704de6802f5e6a6321b8807616
Talha Bay <ihsantalhabay@gmail.com>
2024-12-12 11:42:28 +0200

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,21 @@
+import heroImg from "./assets/hero.svg";
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Necessitatibus natus modi sint tempora molestias iste veritatis enim
+            ab voluptas velit.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
